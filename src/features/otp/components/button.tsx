f044acf05778487c8f6a666127637d3c4caeba93
Endre Ulberg <endre.ulberg@gmail.com>
2022-08-21 22:26:11 +0200

    refactoring

--- src/features/otp/components/button.tsx
+++ src/features/otp/components/button.tsx
@@ -9,31 +9,15 @@ const OtpInputButton = (props: Props) => {
     const [otp,setOtp] = useState<any>();
     const [tempValue,setTempValue] = useState<any>();
     if ('OTPCredential' in window) {
-        window.addEventListener('DOMContentLoaded', e => {
+        window.addEventListener('DOMContentLoaded', async (e) => {
           const ac = new AbortController();
-
-          navigator.credentials.get({
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            otp: { transport:['sms'] },
-            signal: ac.signal
-          }).then(otp => {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            setOtp(otp.code)
-          }).catch(err => {
-            console.log(err);
-          });
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const {type, code} = await navigator.credentials.get({otp: { transport:['sms'] },signal: ac.signal})
+        setOtp(code);
         });
       }
-    return <>
-    <h1> OTP: {otp} </h1>
-    <h1> Input.value: {tempValue} </h1>
-    <form>
-  <input value={otp} onChange={e => setOtp(e.target.value)} required/>
-  <input type="submit"/>
-</form>
-    </>
+    return <input value={otp} onChange={e => setOtp(e.target.value)} required/>
     
 }
 
