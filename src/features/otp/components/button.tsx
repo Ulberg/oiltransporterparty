462243bbd26f75149d5c247c7b8aeccfacda04d4
Endre Ulberg <endre.ulberg@gmail.com>
2022-08-21 22:09:19 +0200

    ls

--- src/features/otp/components/button.tsx
+++ src/features/otp/components/button.tsx
@@ -6,19 +6,17 @@ type Props = {
 
 const OtpInputButton = (props: Props) => {
 
-    const [otp,setOtp] = useState<any>("")
-
+    const [otp,setOtp] = useState<any>();
+    const [tempValue,setTempValue] = useState<any>();
     if ('OTPCredential' in window) {
         window.addEventListener('DOMContentLoaded', e => {
           const input = document.querySelector('input[autocomplete="one-time-code"]');
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+          setTempValue(input.value);
           if (!input) return;
           const ac = new AbortController();
-          const form = input.closest('form');
-          if (form) {
-            form.addEventListener('submit', e => {
-              ac.abort();
-            });
-          }
+
           navigator.credentials.get({
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
@@ -31,14 +29,14 @@ const OtpInputButton = (props: Props) => {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             setOtp(otp.code)
-            if (form) form.submit();
           }).catch(err => {
             console.log(err);
           });
         });
       }
     return <>
-    <h1> {otp} </h1>
+    <h1> OTP: {otp} </h1>
+    <h1> Input.value: {tempValue} </h1>
     <form>
   <input autoComplete="one-time-code" required/>
   <input type="submit"/>
